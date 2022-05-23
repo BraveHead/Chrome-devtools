67ea6a0f7e0f9e5077eb03537f206fe5bdfb58f2
zhangshun <zhangshun@growingio.com>
2022-05-23 17:09:02 +0800

    feat: 测试demo

---
+++ popup.js
@@ -0,0 +1,20 @@
+let changeColorElement = document.getElementById('changeColor');
+
+chrome.storage.sync.get('color', ({ color })=> {
+    changeColorElement.style.backgroundColor = color;
+});
+
+changeColorElement.addEventListener('click', async() => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: setPageBackgroundColor,
+    });
+});
+
+function setPageBackgroundColor() {
+    chrome.storage.sync.get('color', ({ color }) => {
+        document.body.style.backgroundColor = color;
+    })
+}
